c060b923088217b3231909db1137d3cf9b8e0d57
evanermen <elinevanermen@gmail.com>
2015-02-21 15:21:42 +0100

    triangles werken

--- CG project/CG/RayTracer/src/main/Renderer.java
+++ CG project/CG/RayTracer/src/main/Renderer.java
@@ -45,7 +45,7 @@ public class Renderer {
 	public static void main(String[] arguments) {
 		int width = 640;
 		int height = 640;
-		Point cameraOrigin = new Point(-20,-20,2);
+		Point cameraOrigin = new Point(-20 ,-20 ,0);
 		Vector lookAt = new Vector(0.5, 0.5, 0);
 		Vector up = new Vector(0,0,1);
 		double fov = 90;
@@ -106,7 +106,7 @@ public class Renderer {
 		// initialize the scene
 		Transformation turn =  Transformation.createRotationX(30);
 		Transformation turn2 =  Transformation.createRotationZ(23);
-		Transformation turn3 = Transformation.createRotationY(80);
+		Transformation turn3 = Transformation.createRotationY(50);
 		Transformation t1 = Transformation.createTranslation(0, 0, 0).append(turn).append(turn2).append(turn3);
 		Transformation t2 = Transformation.createTranslation(4, -4, 12);
 		Transformation t3 = Transformation.createTranslation(15, 15, 0);
@@ -117,11 +117,11 @@ public class Renderer {
 		Transformation scale = Transformation.createScale(10, 10, 10);
 
 		//world.addObject(new Cube(t1.append(t6).append(turn3), 5));
-		world.addObject(new Triangle(scale.append(turn3).append(turn)));
-		world.addObject(new Cube(identity.append(turn), 2));
+		world.addObject(new Triangle(identity, new Point(0,-10,-10), new Point(0,10,20), new Point(10,0,5)));
+		world.addObject(new Cube(identity, 5));
 		//world.addObject(new Sphere(t4, 4));
 		//world.addObject(new Sphere(t5.append(t6), 4));
-		//world.addObject(new Plane(identity));
+		//world.addObject(new Plane(turn3));
 
 		//initialize the lights
 		
--- CG project/CG/RayTracer/src/shape/Triangle.java
+++ CG project/CG/RayTracer/src/shape/Triangle.java
@@ -31,45 +31,47 @@ public class Triangle extends Shape {
 	
 	@Override
 	public Intersection intersect(Ray ray) {
+		
+		Ray transformed = transformation.transformInverse(ray);
 		double a = v0.x - v1.x;
 		double b = v0.x - v2.x;
-		double c = ray.direction.x;
+		double c = transformed.direction.x;
 		double d = v0.x - ray.origin.x;
 		
 		double e = v0.y - v1.y;
 		double f = v0.y - v2.y;
-		double g = ray.direction.y;
+		double g = transformed.direction.y;
 		double h = v0.y - ray.origin.y;
 		
 		double i = v0.z - v1.z;
 		double j = v0.z - v2.z;
-		double k = ray.direction.z;
-		double l = v0.z - ray.origin.z;
+		double k = transformed.direction.z;
+		double l = v0.z - transformed.origin.z;
 		
 		double m = f*k - g*j;
-		double n = h*k - g*l;
-		double p = f*l - h*j;
+		double n = -h*k + g*l;  //tekenverandering
+		double p = -f*l + h*j;  //tekenverandering
 		double q = g*i - e*k;
 		double s = e*j - f*i;
 		
 		double inv_denom = 1.0/(a*m + b*q + c*s);
 		
-		double e1 = d*m - b*n - c*p;
+		double e1 = d*m + b*n + c*p;
 		double beta = e1*inv_denom;
 		
-		System.out.println("Beta = " + beta);
+		//System.out.println("Beta = " + beta);
 		if(beta < 0) return null;
 		
 		double r = e*l - h*i;
-		double e2 = a*n + d*q + c*r;
+		double e2 = a*n + d*q + c*r; //minteken voor a*n
 		double gamma = e2*inv_denom;
 		
-		System.out.println("Gamma = " + gamma);
+		//System.out.println("Gamma = " + gamma);
 		if(gamma<0) return null;
 		
 		if(beta + gamma > 1) return null;
 		
-		double e3 = a*p - b*r + d*s;
+		double e3 = a*p - b*r + d*s; //minteken voor a*p
 		double t = e3*inv_denom;
 		
 		//if(t< kEpsilon) return null;
@@ -80,7 +82,7 @@ public class Triangle extends Shape {
 
 	@Override
 	public RGBColor getColor(Point point) {
-		return new RGBColor(0,0,255);
+		return new RGBColor(getNormal(point));
 	}
 
 	@Override
@@ -90,8 +92,9 @@ public class Triangle extends Shape {
 
 	@Override
 	public Vector getNormal(Point point) {
-		// TODO Auto-generated method stub
-		return null;
+		Vector divident = v1.subtract(v0).cross(v2.subtract(v0));
+		double divisor = divident.length();
+		return divident.scale(1/divisor);
 	}
 
 }
